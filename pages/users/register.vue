a526454bf792c4de44239e078a9ba4a08ca2d4a9
nattharikanan <natthariknan@gmail.com>
2020-09-15 00:54:58 +0700

    reverse

--- pages/users/register.vue
+++ pages/users/register.vue
@@ -134,6 +134,8 @@
 
 <script>
 import axios from "axios";
+import VuePhoneNumberInput from "vue-phone-number-input";
+import "vue-phone-number-input/dist/vue-phone-number-input.css";
 import VueRecaptcha from "vue-recaptcha";
 
 export default {
